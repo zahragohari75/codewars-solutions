c5e8ad386bf39f9542a69c62875b8dc8307aac73
zahragohari75 <zahragohari75@outlook.com>
2025-11-14 15:17:08 +0330

    Remove old 7-kyu (without link)

--- 7-kyu/README.md
+++
@@ -1,5 +0,0 @@
-# 7 kyu Solutions
-
-| Kata | Solution | Language |
-|------|----------|----------|
-| Credit Card Mask | [credit_card_mask.py](credit_card_mask.py) | Python |
--- 7-kyu/credit_card_mask.py
+++
@@ -1,8 +0,0 @@
-def maskify(cc):
-
-    if len(cc) <= 4:
-        return cc
-    else:
-        return "#" * (len(cc)-4) + cc[-4:]# 7 kyu - Credit Card Mask
-# https://www.codewars.com/kata/...
-
