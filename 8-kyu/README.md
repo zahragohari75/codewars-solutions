d170b896261d6c2a9461286ef498a1a1f86e68ed
zahragohari75 <zahragohari75@outlook.com>
2025-11-14 16:28:50 +0330

    Add 8 kyu: Sum Arrays

---
+++ 8-kyu/README.md
@@ -0,0 +1,5 @@
+# 8 kyu Solutions
+
+| Kata | Solution | Language |
+|------|----------|----------|
+| [Sum Arrays](https://www.codewars.com/kata/53dc54212259ed3d4f00071c) | [sum_array.py](sum_array.py) | Python |
---
+++ 8-kyu/sum_array.py
@@ -0,0 +1,15 @@
+# 8 kyu - Sum Arrays
+# https://www.codewars.com/kata/53dc54212259ed3d4f00071c
+
+def sum_array(a):
+    return sum(a)
+
+#____ TEST __________________________________
+Input: [1, 5.2, 4, 0, -1]
+Output: 9.2
+
+Input: []
+Output: 0
+
+Input: [-2.398]
+Output: -2.398
